4433fbb90deda53e54d1756128b8022f5e240858
Johan Mora <johan.morar@udea.edu.co>
2020-12-12 22:24:31 -0500

    Avance 1 en el codigo

--- Parcial2.pro
+++ Parcial2.pro
@@ -4,4 +4,8 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
+        bala.cpp \
         main.cpp
+
+HEADERS += \
+    bala.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
-
-using namespace std;
+#include "bala.h"
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float Ho, Hd, d;
+    cout<<"Digite la altura del canion ofensivo: "<<endl; cin>>Ho;
+    cout<<"Digite la altura del canion defensivo: "<<endl; cin>>Hd;
+    cout<<"Digite la distancia que sepra los caniones: "<<endl;cin>>d;
+    Bala Bo(0.05*d,0,Ho);
+    Bala Bd(0.025*d,d,Hd);
+    Bala Bn(0.005*d,0,Ho);
     return 0;
 }
