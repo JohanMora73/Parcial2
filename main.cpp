e6509880284286cbb758712073eedda30168e514
Johan Mora <johan.morar@udea.edu.co>
2020-12-13 00:53:27 -0500

    Avance 3 en el codigo fuente:	Se implementa una funcion que permite dar solucion al numeral 3 del problema propuesto

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "bala.h"
-void GenerarDisparo(Bala B_, int x_, int y_);
+void GenerarDisparos(Bala B_, int x_, int y_);
+void GenerarDispDefensivos(Bala B1,Bala B2,int d, int Hd);
 int main()
 {
-    float Ho, Hd, d;
-    int menu, cont=0;
-    bool resp=false;
+    float Ho, Hd, d, a, vo;
+    int menu;
     cout<<"Digite la altura del canion ofensivo: "<<endl; cin>>Ho;
     cout<<"Digite la altura del canion defensivo: "<<endl; cin>>Hd;
     cout<<"Digite la distancia que sepra los caniones: "<<endl;cin>>d;
@@ -13,41 +13,31 @@ int main()
     Bala Bn(0.005*d,0,Ho,1);
     cout<<"(1) Generar 3 disparos ofensivos que comprometan la integridad del canion defensivo."<<endl;
     cout<<"(2) Generar 3 disparos defensivos que comprometan la integridad del canion ofensivo."<<endl;
+    cout<<"(3) Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el canion defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido."<<endl;
     cin>>menu;
     switch(menu){
         case 1:
-            GenerarDisparo(Bo,d,Hd);
-        /*
-            while(cont<3){
-                Bo.AnguloAleatorio();
-                for(int v0 = 0;v0<100;v0+=2){
-                    Bo.VelocidadX(Bo.angulo,v0);
-                    Bo.VelocidadY(Bo.angulo,v0);
-                    for(int t=0;t<=20;t++){
-                        Bo.PosicionX(t);
-                        Bo.PosicionY(t);
-                        if(sqrt(pow(Bo.x-d,2)+pow(Bo.y-Hd,2))<=Bo.radio){
-                            cout<<"angulo de riezgo "<<Bo.angulo*180/Bo.pi<<" y velocidad: "<<v0<<"Tiempo de vuelo: "<<t<<endl;
-                            cont++;
-                            resp=true;
-                            break;
-                        }
-                    }
-                    if(resp==true) {resp=false; break;}
-                }
-            }*/
+            GenerarDisparos(Bo,d,Hd);
         break;
 
         case 2:
-            GenerarDisparo(Bd,0,Ho);
+            GenerarDisparos(Bd,0,Ho);
+        break;
+
+        case 3:
+            cout<<"ingrese los datos del disparo del canion ofensivo, angulo (en grados) y velocidad inicial respectivamente: "<<endl;
+            cin>>a>>vo;
+            Bo.angulo=a*Bo.pi/180;
+            Bo.vx=vo*cos(Bo.angulo);
+            Bo.vy=vo*sin(Bo.angulo);
+            GenerarDispDefensivos(Bo,Bd,d, Hd);
         break;
     }
 
     return 0;
 }
 
-void GenerarDisparo(Bala B_, int x, int y){
-    Bala B=B_;
+void GenerarDisparos(Bala B, int x, int y){
     int cont=0;
     bool resp=false;
     while(cont<3){
@@ -59,7 +49,7 @@ void GenerarDisparo(Bala B_, int x, int y){
                 B.PosicionX(t);
                 B.PosicionY(t);
                 if(sqrt(pow(B.x-x,2)+pow(B.y-y,2))<=B.radio){
-                    cout<<"angulo de riezgo "<<B.angulo*180/B.pi<<" y velocidad: "<<v0<<"Tiempo de vuelo: "<<t<<endl;
+                    cout<<"angulo de riezgo: "<<B.angulo*180/B.pi<<" Velocidad inicial: "<<v0<<" Tiempo de vuelo: "<<t<<" Coordenada final x: "<<B.x<<" Coordenada final y: "<<B.y<<endl;
                     cont++;
                     resp=true;
                     break;
@@ -68,4 +58,42 @@ void GenerarDisparo(Bala B_, int x, int y){
             if(resp==true) {resp=false; break;}
         }
     }
+}
+
+void GenerarDispDefensivos(Bala Bo,Bala Bd, int d, int Hd){
+    bool riezgo,resp;
+    int cont=0;
+    for(int t=0;t<30;t++){
+        Bo.PosicionX(t);
+        Bo.PosicionY(t);
+        if(sqrt(pow(Bo.x-d,2)+pow(Bo.y-Hd,2))<=Bo.radio){
+            riezgo=true;
+            break;
+        }
+        else riezgo=false;
+    }
+    if(riezgo==true){
+        cout<<"El disparo representa un riezgo"<<endl;
+        while(cont<3){
+            Bd.AnguloAleatorio();
+            for(int vo = 0;vo<100;vo+=2){
+                Bd.VelocidadX(Bd.angulo,vo);
+                Bd.VelocidadY(Bd.angulo,vo);
+                for(int t=0;t<30;t++){
+                    Bo.PosicionX(t+2);
+                    Bo.PosicionY(t+2);
+                    Bd.PosicionX(t);
+                    Bd.PosicionY(t);
+                    if(sqrt(pow(Bo.x-Bd.x,2)+pow(Bo.y-Bd.y,2))<=Bd.radio && sqrt(pow(Bo.x-d,2)+pow(Bo.y-Hd,2))>Bo.radio){
+                        cout<<"Un disparo defensivo ceretero es: angulo"<<Bd.angulo*180/Bd.pi<<", velovidad: "<<-Bd.vx/cos(Bd.angulo)<<" tiempo de vuelo: "<<t<<" Coordenada final x: "<<Bd.x<<" Coordenada final y: "<<Bd.y<<endl;
+                        cont++;
+                        resp=true;
+                        break;
+                    }
+                }
+                if(resp==true) {resp=false; break;}
+            }
+        }
+    }
+    else cout<<"El disparo no representa riezagos"<<endl;
 }
